5f6cd34440b8650da7c35f107c13fbc8eb8dd337
angelicrf <angelicrf@gmail.com>
2020-08-14 12:50:30 -0700

    packages installed

--- weatherapp/lib/main.dart
+++ weatherapp/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 void main() => {
   runApp(
@@ -27,6 +30,8 @@ class _HomeState extends State<Home>{
             width: MediaQuery.of(context).size.width,
             color: Colors.red,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Padding(
                   padding: EdgeInsets.only(bottom: 10.0),
@@ -51,6 +56,35 @@ class _HomeState extends State<Home>{
                 ),
 
               ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.thermometerHalf),
+                    title: Text("Temperature"),
+                    trailing: Text("52\u00B0") ,
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.cloud),
+                    title: Text("Weather"),
+                    trailing: Text("Weather") ,
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.sun),
+                    title: Text("Temperature humidity"),
+                    trailing: Text("12") ,
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.wind),
+                    title: Text("Wind Speed"),
+                    trailing: Text("12") ,
+                  )
+                ],
+              ),
             ),
           )
         ],
